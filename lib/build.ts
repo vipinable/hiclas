34b5770b44ed803c341f4ef17af267f5c81080b0
vipinable <vipinable@gmail.com>
2023-09-24 13:26:01 +0200

    update cdk version

--- lib/build.ts
+++ lib/build.ts
@@ -3,8 +3,8 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
-import * as origins from 'aws-cloudfront-origins' 
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'; 
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
 export class LambdaWithLayer extends Stack {
