a119989277c1d697533c4e37d98abc68c74135a7
vipinable <vipinable@gmail.com>
2024-02-18 10:25:33 +0100

    check varable value

--- lib/build.ts
+++ lib/build.ts
@@ -116,7 +116,7 @@ export class LambdaWithLayer extends Stack {
       defaultRootObject: 'index.html'
     });
 
-    console.log(mainfnUrl.url.toString())
+    console.log(mainfnUrl.url)
 
     // const fnUrlOrigin = new origins.HttpOrigin(mainfnUrl.url.toString().split("/")[2])
 
