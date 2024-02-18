4d71b42c9438ea2ad25200bd92ff753c0dc8ac6a
vipinable <vipinable@gmail.com>
2024-02-18 14:44:08 +0100

    read string value

--- lib/build.ts
+++ lib/build.ts
@@ -123,16 +123,16 @@ export class LambdaWithLayer extends Stack {
       defaultRootObject: 'index.html'
     });
 
-    this.fnUrl = ssm.StringParameter.fromStringParameterAttributes(this, 'MyValue', {
-        parameterName: fnUrlParam.parameterName,
-    }).stringValue;
+    // this.fnUrl = ssm.StringParameter.fromStringParameterAttributes(this, 'MyValue', {
+    //     parameterName: fnUrlParam.parameterName,
+    // }).stringValue;
 
     // const TheUrl = new CfnOutput(this, 'TheUrl', {
     //   // The .url attributes will return the unique Function URL
     //   value: this.fnUrl,
     // });
 
-    // console.log())
+     console.log(fnUrlParam.stringValue)
 
 
     // const fnUrlOrigin = new origins.HttpOrigin(mainfnUrl)
