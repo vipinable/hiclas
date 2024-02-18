e31c02c5d8f507e895748923761fdddf8d01b3c4
vipinable <vipinable@gmail.com>
2024-02-18 18:36:49 +0100

    test ouput

--- lib/build.ts
+++ lib/build.ts
@@ -131,7 +131,7 @@ export class LambdaWithLayer extends Stack {
 
     const TheUrl = new CfnOutput(this, 'TheUrl', {
       // The .url attributes will return the unique Function URL
-      value: this.fnUrl.split('/'),
+      value: this.fnUrl.split('/')[0],
     });
 
     //  console.log(this.fnUrl)
