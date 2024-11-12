207ffe3c99169536cb02fca9b25f97d0d391e3ac
vipinable <vipinable@gmail.com>
2024-11-12 21:42:31 +0100

    fix error

--- lib/build.ts
+++ lib/build.ts
@@ -126,7 +126,7 @@ export class LambdaWithLayer extends Stack {
     /**
      * Behavior for CSS files
      */
-    hiclasDist.addBehavior('/css/*', new origins.S3BucketOrigin(hiclastore), {
+    hiclasDist.addBehavior('/css/*', new origins.S3Origin(hiclastore), {
       cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
       viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
     });
@@ -134,7 +134,7 @@ export class LambdaWithLayer extends Stack {
     /**
      * Behavior for images
      */
-    hiclasDist.addBehavior('/images/*', new origins.S3BucketOrigin(hiclastore), {
+    hiclasDist.addBehavior('/images/*', new origins.S3Origin(hiclastore), {
       cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
       viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       compress: true
