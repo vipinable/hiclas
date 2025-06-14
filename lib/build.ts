e1da9de917e609d69aa616934cf9cdc4222f949a
vipinable <vipinable@gmail.com>
2025-06-14 21:07:35 +0200

    Disable edge function

--- lib/build.ts
+++ lib/build.ts
@@ -11,7 +11,7 @@ import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'; 
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
-import { EdgeFunction } from 'aws-cdk-lib/aws-cloudfront/lib/experimental';
+// import { EdgeFunction } from 'aws-cdk-lib/aws-cloudfront/lib/experimental';
 export class LambdaWithLayer extends Stack {
   public fnUrl: string
 
