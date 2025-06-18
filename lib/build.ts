6f23a46eb1fcceb6418d65da5d279b1ae066ccd4
vipinable <vipinable@gmail.com>
2025-06-18 23:11:55 +0200

    tweak beheavior

--- lib/build.ts
+++ lib/build.ts
@@ -269,12 +269,11 @@ export class LambdaWithLayer extends Stack {
     hiclasapi.root.addMethod('GET', hiclasapiIntegration);
 
     //Add beheavior for api gateway and forward requests to apigateway
-    hiclasDist.addBehavior('/api/*', new origins.HttpOrigin(hiclasapi.url.split('/')[2]), {
+    hiclasDist.addBehavior('/api/*', new origins.HttpOrigin(Fn.parseDomainName(hiclasapi.url.split('/')[2])), {
       viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
       cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
       originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
-      compress: true,
     });
 
     //export the apiUrl as a CfnOutput
