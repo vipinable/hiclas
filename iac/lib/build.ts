ffc681c24ca5837d2d3115ff497f802fe0926f55
vipinable <vipinable@gmail.com>
2025-06-29 11:31:00 +0200

    fix syntax

--- iac/lib/build.ts
+++ iac/lib/build.ts
@@ -251,7 +251,7 @@ export class LambdaWithLayer extends Stack {
      * Deploy assets files to the S3 bucket.
      */
     new s3deploy.BucketDeployment(this, 'DeployAssets', {
-      sources: [s3deploy.Source.asset('../dist/')]
+      sources: [s3deploy.Source.asset('../dist/')],
       destinationBucket: hiclastore,
       // destinationKeyPrefix: 'assets/',
       prune: false, // Set to true to remove files not in the source
