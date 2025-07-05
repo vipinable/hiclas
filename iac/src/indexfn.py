095a587e577e7de47a230284e40b4d9294a84ecf
vipinable <vipinable@gmail.com>
2025-07-05 22:22:36 +0200

    cors headers

--- iac/src/indexfn.py
+++ iac/src/indexfn.py
@@ -84,7 +84,12 @@ def handler(event, context):
             return({
                 'statusCode': '200',
                 'body': response,
-                'headers': {'Content-Type': 'application/json'}
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET,POST,PUT,OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+                    }
             })
         else:
             return({
